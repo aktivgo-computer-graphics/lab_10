70851681d2fa0077518ca0c0831dc677053d0396
aktivgo <aktivgo01062001@gmail.com>
2022-05-06 11:12:44 +0300

    Init lab

---
+++ task/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace task
+{
+    public partial class MainForm : Form
+    {
+        private Pen MyPen;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            MyPen = new Pen(Color.Black);
+        }
+        
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
